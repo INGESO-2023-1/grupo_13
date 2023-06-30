08a1e0c2875ee160f5700364dcdb73cf01436dfc
Adolfo Espinosa <adolfo.espinosa@sansano.usm.cl>
2023-06-29 22:01:41 -0400

    update front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
-
+import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Logout from "./pages/Logout";
-import Contacts from "./pages/Contacts";
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Chat />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
